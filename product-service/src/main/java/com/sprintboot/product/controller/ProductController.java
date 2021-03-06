06108a5fe112fc4e30cfc60d64b3395bfcdc3711
Sampath <sampath.dapanadurage@ncs.com.sg>
2021-03-06 13:56:34 +0800

    adds extra swagger annotations

--- product-service/src/main/java/com/sprintboot/product/controller/ProductController.java
+++ product-service/src/main/java/com/sprintboot/product/controller/ProductController.java
@@ -2,6 +2,9 @@ package com.sprintboot.product.controller;
 
 import com.sprintboot.product.model.Product;
 import com.sprintboot.product.service.ProductService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,42 +27,49 @@ import java.util.List;
 @RequestMapping(value = "v1")
 @RequiredArgsConstructor
 @Validated
+@Api(description = "Product API endpoints")
 public class ProductController {
     private final ProductService productService;
 
     @PostMapping(value = "product")
-    ResponseEntity<Product> addProduct(@Valid @RequestBody Product productDTO) {
+    @ApiOperation("Adds product endpoint")
+    ResponseEntity<Product> addProduct(@ApiParam("product details") @Valid @RequestBody Product productDTO) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(this.productService.addProduct(productDTO));
     }
 
     @GetMapping(value = "productList")
+    @ApiOperation("Retrieves products list endpoint")
     ResponseEntity<List<Product>> productList() {
         return ResponseEntity.ok()
                 .body(this.productService.productList());
     }
 
     @GetMapping(value = "productList/{category}")
-    ResponseEntity<List<Product>> productCategoryList(@PathVariable @NotNull String category) {
+    @ApiOperation("Retrieves product by category endpoint")
+    ResponseEntity<List<Product>> productCategoryList(@ApiParam("category of the products") @PathVariable @NotNull String category) {
         return ResponseEntity.ok()
                 .body(this.productService.productCategoryList(category));
     }
 
     @GetMapping(value = "product/{id}")
-    ResponseEntity<Product> productCategoryList(@PathVariable @Min(0) Integer id) {
+    @ApiOperation("Retrieves product by id endpoint")
+    ResponseEntity<Product> productCategoryList(@ApiParam("id of the product") @PathVariable @Min(0) Integer id) {
         return ResponseEntity.ok()
                 .body(this.productService.productById(id));
     }
 
     @PutMapping(value = "product/{id}")
-    ResponseEntity<Product> updateProduct(@Valid @RequestBody Product productDTO, @PathVariable @NotNull Integer id) {
+    @ApiOperation("Updates product endpoint")
+    ResponseEntity<Product> updateProduct(@ApiParam("product details") @Valid @RequestBody Product productDTO,@ApiParam("id of the product") @PathVariable @NotNull Integer id) {
         productDTO.setId(id);
         return ResponseEntity.ok()
                 .body(this.productService.updateProduct(productDTO));
     }
 
     @DeleteMapping(value = "product/{id}")
-    ResponseEntity<Void> deleteProduct(@PathVariable @NotNull Integer id) {
+    @ApiOperation("Deletes product endpoint")
+    ResponseEntity<Void> deleteProduct(@ApiParam("id of the product") @PathVariable @NotNull Integer id) {
         this.productService.deleteProduct(id);
         return ResponseEntity.noContent().build();
     }
--- product-service/src/main/java/com/sprintboot/product/model/Product.java
+++ product-service/src/main/java/com/sprintboot/product/model/Product.java
@@ -1,5 +1,7 @@
 package com.sprintboot.product.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.ToString;
@@ -18,22 +20,38 @@ import java.util.List;
 @Builder
 @ToString
 @Document(collection = "products")
+@ApiModel("Contains all attributes of the product")
 public class Product {
     @Transient
     public static final String SEQUENCE_NAME = "products_sequence";
+
+    @ApiModelProperty("Unique id of the product")
     @Id
     private Integer id;
+
+    @ApiModelProperty("Unique id of the product")
     @NotNull(message = "Product name should not be null")
     private String name;
+
     @Valid
     @NotNull(message = "Product category should not be null.....")
     private Category category;
+
+    @ApiModelProperty("Currency type")
     private String currency;
     @Min(0)
+
+    @ApiModelProperty("Price of the product")
     private BigDecimal price;
+
+    @ApiModelProperty("Discount of the product")
     @Max(100)
     @Min(0)
     private BigDecimal discount;
+
+    @ApiModelProperty("Discount description of the product")
     private String discountDescription;
+
+    @ApiModelProperty("Images url of the product")
     private List<String> imageURLs;
 }
